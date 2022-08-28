5da63bdf70ec8f52afedbae018ae6af99468f618
NasirKhatri <45204768+NasirKhatri@users.noreply.github.com>
2022-08-28 21:11:20 +0500

    Wrote SQL

--- routes/subscription.js
+++ routes/subscription.js
@@ -17,22 +17,36 @@ subscribeRouter.post('/', (req, res) => {
     const PhoneNo = req.body.PhoneNo;
     const RetypePassword = req.body.RetypePassword;
     const Voucher = req.body.Voucher;
+    const Package = req.body.Package;
 
-    //SQL for checking if the requested email or phonenumber already exist
+    var today = new Date();
+    const RegistrationDate = today.getFullYear()+'-'+("0" + (today.getMonth()+1)).slice(-2)+'-'+("0" + today.getDate()).slice(-2);
+    var ValidityDate = new Date(new Date().setDate(today.getDate() + 31));
+    ValidityDate = ValidityDate.getFullYear()+'-'+("0" + (ValidityDate.getMonth()+1)).slice(-2)+'-'+("0" + ValidityDate.getDate()).slice(-2);
+
     const SQLForClientExistanceCheck = `SELECT EXISTS(SELECT * FROM clients WHERE Email = '${Email}' OR PhoneNumber = '${PhoneNo}') AS 'Count'`;
+    const SQlForClientRegistration = `INSERT INTO clients (ClientID, Name, Email, PhoneNumber, RegisteredOn, Package, ClientType, Validity)
+                                      VALUES (NULL, '${Name}', '${Email}', '${PhoneNo}', '${RegistrationDate}', '${Package}', 'Regular', '${ValidityDate}')`;
 
     (async () => {
         try {
             const userCheck = await query(SQLForClientExistanceCheck);
-            if(userCheck[0].Count) {
+            if (userCheck[0].Count) {
                 res.status(400).send('User Already Exist');
                 return;
             }
-            res.send(userCheck);
-            return;
+            const result = await query(SQlForClientRegistration);
+            if(result.affectedRows === 1) {
+                //To Be Continue From Here
+                //
+                //
+                res.status(200).send(result);
+                return;
+            }
         }
         catch (err) {
             res.send(err);
+            return;
         }
     })();
 })
