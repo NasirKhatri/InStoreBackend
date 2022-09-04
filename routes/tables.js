8f7ee6a3b449205bbaa41099262a0c759e5e2f92
NasirKhatri <45204768+NasirKhatri@users.noreply.github.com>
2022-09-04 16:29:52 +0500

    Add Table API completed

--- routes/tables.js
+++ routes/tables.js
@@ -1,8 +1,53 @@
 const express = require('express');
 const tablesRouter = express.Router();
+const util = require('util');
+const verifyToken = require("../functions/userVarification");
+const authorization = require('../functions/authorization');
 
-tablesRouter.get('/', (req, res) => {
-    res.send('Tables Router is responding');
+//Importing Database Connection
+const db = require('../dbConnection');
+const query = util.promisify(db.query).bind(db);
+
+
+tablesRouter.post('/', verifyToken, authorization, (req, res) => {
+
+    //destructuring request body
+    const clientID = parseInt(req.body.clientID);
+    const userID = parseInt(req.body.userID);
+    const BranchID = req.body.BranchID;
+    const TableName = req.body.TableName;
+
+
+    (async () => {
+
+        try {
+            //Create Local Table ID
+            const SQL1 = `SELECT COUNT(ClientID) AS 'Tables' FROM tables WHERE ClientID = ${clientID};`;
+            let counter = await query(SQL1);
+            counter = counter[0].Tables + 1;
+            const localTableID = 'TB' + ('00' + counter).slice(-3);
+
+            //SQL for Adding Table in the database
+            const SQL2 = `INSERT INTO tables (TableID, LocalTableID, ClientID, TableName, BranchID) 
+                            VALUES (NULL, '${localTableID}', ${clientID}, '${TableName}', '${BranchID}')`;
+            const addedTable = await query(SQL2);
+            console.log(addedTable);
+
+            if (addedTable.affectedRows > 0) {
+                res.status(200).json({ msg: "Table have been added" });
+            }
+            else {
+                res.status(500).json({ msg: "Something went wrong" });
+            }
+        }
+        
+        catch (err) {
+            res.status(500).json({ msg: "Something went wrong" });
+            return;
+        }
+    })()
+
+
 });
 
 module.exports = tablesRouter;
