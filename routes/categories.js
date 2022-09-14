8ee26d43940b2292fbd76a56fc9ac5d097279252
NasirKhatri <45204768+NasirKhatri@users.noreply.github.com>
2022-09-14 19:42:31 +0500

    Add Category api bug of parsing client id to int have been resolved

--- routes/categories.js
+++ routes/categories.js
@@ -15,35 +15,25 @@ const query = util.promisify(db.query).bind(db);
 // File Uploading through Multer
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const path = `./uploads/${req.body.clientID}/categories`;
+        const path = `./uploads/${parseInt(req.body.clientID)}/categories`;
         fs.mkdirSync(path, { recursive: true });
         return cb(null, path);
     },
     filename: function (req, file, cb) {
-        cb(null, req.body.clientID + "_" + "Category_" + file.originalname.replace(".", "_") + "_" + Date.now() + path.extname(file.originalname)) //Appending extension
+        cb(null, parseInt(req.body.clientID) + "_" + "Category_" + file.originalname.replace(".", "_") + "_" + Date.now() + path.extname(file.originalname)) //Appending extension
     }
 })
 
 var upload = multer({ storage: storage });
 
-/*function authorization(req, res, next) {
-    //Only Admins are allowed to add category
-    const roleID = parseInt(req.authData.roleID);
-    if (roleID !== 1) res.status(403).json({ msg: "Sorry your are not authorized to add categories" });
-    else {
-        next();
-    }
-}*/
-
-categoriesRouter.post('/addcategory', verifyToken, (req, res, next) => authorization(req, res, next), upload.single('Image'), (req, res) => {
-
+categoriesRouter.post('/addcategory', verifyToken, authorization, upload.single('Image'), (req, res) => {
     //destructuring request body
     const clientID = parseInt(req.body.clientID);
     const userID = parseInt(req.body.userID);
     const name = req.body.Name;
     const branches = req.body.Branches;
     const color = req.body.Color;
-    let imageSource = `uploads/${req.body.clientID}/categories/${req.file.filename}`;
+    let imageSource = `uploads/${clientID}/categories/${req.file.filename}`;
     const imageInPOS = req.body.ImageInPOS;
     const visibilityInPOS = req.body.VisibilityInPOS;
 
